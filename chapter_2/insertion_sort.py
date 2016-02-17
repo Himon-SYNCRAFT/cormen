5ffd9700ecad218f295009c529c81f1cc2ed5ad4
Himon <d.zawlocki@eltrox.pl>
2016-02-17 22:36:34 +0100

    2.1-2 Sorting numbers in descending order using insertion sort.

--- chapter_2/insertion_sort.py
+++ chapter_2/insertion_sort.py
@@ -1,9 +1,38 @@
+"""
+Insertion sort implementation in python
+"""
+
 def insertion_sort(numbers):
-    pass
+    """
+    Sorting numbers in descending order
+    """
+    for i in range(1, len(numbers)):
+        r_index = i
+
+        for j in range(i - 1, -1, -1):
+
+            if numbers[r_index] <= numbers[j]:
+                break
+
+            else:
+                numbers[r_index], numbers[j] = numbers[j], numbers[r_index]
+                r_index = j
 
 
 if __name__ == "__main__":
 
-    numbers = []
+    numbers = [1, 2, 3, 4, 5]
     insertion_sort(numbers)
-    print(numbers)
+    assert numbers == [5, 4, 3, 2, 1], "simple sort"
+
+    numbers = [1, 1, 1, 1, 1]
+    insertion_sort(numbers)
+    assert numbers == [1, 1, 1, 1, 1]
+
+    numbers = [3, 2, 1, 4, 5]
+    insertion_sort(numbers)
+    assert numbers == [5, 4, 3, 2, 1]
+
+    numbers = [5, 4, 3, 2, 1]
+    insertion_sort(numbers)
+    assert numbers == [5, 4, 3, 2, 1]
